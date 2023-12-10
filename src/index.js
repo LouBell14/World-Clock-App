e4853c6bd69b08114fd778baa67859314cd7e1f3
LouBell14 <87602844+LouBell14@users.noreply.github.com>
2023-12-10 08:26:06 +0000

    Corrected bug line 28

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+losAngelesTimeElement.innerHTML = "1:59:59";
