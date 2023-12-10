bc935b6801774b8616ab152a6051e3f4fdf72ed6
LouBell14 <87602844+LouBell14@users.noreply.github.com>
2023-12-10 11:34:30 +0000

    Debugged JS

--- src/index.js
+++ src/index.js
@@ -28,19 +28,18 @@ function updateCity(event) {
   let cityTimeZone = event.target.value;
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  let citiesSelectElement = document.querySelector("#cities");
+  let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-   <div class="city">
-          <div>
-            <h2>${cityName}</h2>
-            <div class="date">${city.Time.format("MMMMM Do YYYY")}</div>
-          </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )}<small>${cityTime.format("A")}
-          </small></div>
-        </div>
-        `;
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
 
 updateTime();
